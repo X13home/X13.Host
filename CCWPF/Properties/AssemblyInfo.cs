bdd86a5de203b24d5fb50f28d34fdeb84d6f98ea
Wassili-Hense <hense.wassili@googlemail.com>
2013-04-14 22:24:47 +0200

    PLC/declarers FIX devices serial port declaration

--- CCWPF/Properties/AssemblyInfo.cs
+++ CCWPF/Properties/AssemblyInfo.cs
@@ -61,5 +61,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.1315.35")]
-[assembly: AssemblyFileVersion("0.3.1315.35")]
+[assembly: AssemblyVersion("0.3.1315.39")]
+[assembly: AssemblyFileVersion("0.3.1315.39")]
