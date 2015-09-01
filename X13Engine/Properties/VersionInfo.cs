8fdd3b47f494dfc96c9a1d58589c12974ca1dde4
Wassili Hense <hense.wassili@googlemail.com>
2015-09-01 14:04:48 +0200

    MQTT-SN UPD declarers of devices

--- X13Engine/Properties/VersionInfo.cs
+++ X13Engine/Properties/VersionInfo.cs
@@ -26,5 +26,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.315.901.17")]
-[assembly: AssemblyFileVersion("0.315.901.17")]
+[assembly: AssemblyVersion("0.315.901.18")]
+[assembly: AssemblyFileVersion("0.315.901.18")]
