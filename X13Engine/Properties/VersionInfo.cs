6cd2791434d2793c96332ca3ca165f2e14154988
Wassili Hense <hense.wassili@googlemail.com>
2015-09-01 11:46:11 +0200

    engine ADD BuiltInStatement.Fade

--- X13Engine/Properties/VersionInfo.cs
+++ X13Engine/Properties/VersionInfo.cs
@@ -26,5 +26,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.315.901.3")]
-[assembly: AssemblyFileVersion("0.315.901.3")]
+[assembly: AssemblyVersion("0.315.901.17")]
+[assembly: AssemblyFileVersion("0.315.901.17")]
